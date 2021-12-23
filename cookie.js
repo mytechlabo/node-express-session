3ebb2bdf4dd1f7cd2e0684bc39c2f3a5e56e455b
toan.trinh <toan.trinh@ntq-solution.com.vn>
2021-12-23 09:11:36 +0700

    cookie-sample

---
+++ cookie.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cookieParser());
+const PORT = process.env.PORT || 3050;
+
+app.get("/get-cookie", (req, res) => {
+  // const cookies = req.headers.cookie; // No using cookie-parser
+  const cookies = req.cookies;
+  res.send(cookies);
+});
+
+app.get("/set-cookie", (req, res) => {
+  // res.setHeader("set-cookie", "cart={id: 10, name: 'banh pao', price: 1000}"); // No using cookie-parser
+
+  res
+    .cookie("blog", "https://viblo.asia/", {
+      maxAge: 5 * 1000,
+      // expires: new Date(Date.now() + 5 * 1000),
+      httpOnly: false,
+    })
+    .cookie("cart", "{id: 10, name: 'banh pao', price: 1000}", {
+      maxAge: 5 * 1000,
+      httpOnly: true,
+      secure: true,
+    });
+  res.send("Set OK!");
+});
+
+app.get("/del-cookie", (req, res) => {
+  res.send("Del OK!");
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running ${PORT}`);
+});
